44500d58694ee89be23bf3ff2a57aa0e82180654
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2021-06-22 15:50:14 -0400

    Basic question sending and checking

--- bot.py
+++ bot.py
@@ -16,7 +16,10 @@ async def on_message(message):
         return
         
     if message.channel.id in games:  #The message was sent in a channel with an active game
-        #Check answer
+        if games[message.channel.id].check_answer(message.content):
+            await message.channel.send('That is the correct answer :partying_face: ')
+        else:
+            await message.channel.send('That was not the correct answer')
 
     if message.content.startswith(prefix+'hello'):
         await message.channel.send('Hello!')
@@ -24,9 +27,13 @@ async def on_message(message):
     if message.content == prefix + 'start':
         if message.channel.id in games:
             await message.channel.send('Trivia is already running here!')
+            return
         else:
-            games[message.channel.id] = trivgame.trivgame(message.channel.id)
             await message.channel.send('Loading trivia...')
+            cur_game = trivgame.trivgame(message.channel.id)
+            games[message.channel.id] = cur_game
+            cur_game.grab_new_question()
+            await message.channel.send(cur_game.get_cur_quesiton())
 
 
 client.run(credentials.oauth2_token)
--- trivgame.py
+++ trivgame.py
@@ -1,6 +1,18 @@
 class trivgame:
     def __init__(self, channel_ID):
         self.channel = channel_ID
-        self.question = ''
-        self.answers = []
-        self.hints = []
+        self.question_type = "Standard"
+        self.question = "Test question"
+        self.answers = ["test answer"]
+        self.hints = ["Hints did not initialize correctly", 
+                            "Hints did not initialize correctly 2",
+                            "Hints did not initialize correctly 3"]
+        
+    def grab_new_question(self):
+        return
+        
+    def check_answer(self, guess):
+        return guess.lower() in self.answers
+        
+    def get_cur_quesiton(self):
+        return self.question
