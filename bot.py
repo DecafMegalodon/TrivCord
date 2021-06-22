08f11134665de81392be37fe00de77d9f0c9f672
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2021-06-22 15:26:39 -0400

    Skeleton for start

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import credentials
+import trivgame
 
 client = discord.Client()
 games = {}
@@ -16,13 +17,16 @@ async def on_message(message):
         
     if message.channel.id in games:  #The message was sent in a channel with an active game
         #Check answer
-        print("Active channel")
 
     if message.content.startswith(prefix+'hello'):
         await message.channel.send('Hello!')
         
     if message.content == prefix + 'start':
-        await message.channel.send('This is where I would put my trivia game... IF I HAD ONE')
+        if message.channel.id in games:
+            await message.channel.send('Trivia is already running here!')
+        else:
+            games[message.channel.id] = trivgame.trivgame(message.channel.id)
+            await message.channel.send('Loading trivia...')
 
 
 client.run(credentials.oauth2_token)
---
+++ trivgame.py
@@ -0,0 +1,6 @@
+class trivgame:
+    def __init__(self, channel_ID):
+        self.channel = channel_ID
+        self.question = ''
+        self.answers = []
+        self.hints = []
